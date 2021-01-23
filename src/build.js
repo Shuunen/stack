17b9caf55f4fac1b99892c6b517c7bb10221e5cf
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-01-23 13:31:10 +0100

    add watch & silent features to build

--- src/build.js
+++ src/build.js
@@ -1,14 +1,19 @@
 const tsup = require('tsup').build
+const { logger } = require('./logger')
 
-function build(options) {
-  if (options === undefined || options.length === 0) throw new Error('can\'t build without input, ex: stack build src/my-file.ts')
+async function build(options) {
+  if (options === undefined || options.length === 0) throw new Error('can\'t build without input')
   const input = options[0]
   options = options.join(' ')
   const outDir = (/--out-dir (\S*)/.exec(options) || [null, 'dist'])[1]
   const format = (/--format (\S*)/.exec(options) || [null, 'cjs'])[1]
   const minify = options.includes('--minify')
-  const config = { outDir, minify, entryPoints: [input], format: [format] }
-  tsup(config)
+  const watch = options.includes('--watch')
+  const verbose = !options.includes('--silent')
+  if (verbose) logger.consoleLogAllowed = true
+  const config = { outDir, minify, watch, entryPoints: [input], format: [format] }
+  await tsup(config)
+  logger.consoleLogAllowed = false
 }
 
 exports.build = build
