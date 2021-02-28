34de370a0744fea2df840000db91ced4262ac643
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-02-28 12:38:00 +0100

    fix path

--- src/lint.js
+++ src/lint.js
@@ -1,8 +1,9 @@
 const { spawn } = require('child_process')
 const { logger } = require('./logger')
+const path = require('path')
 
 function lint() {
-  const filepath = 'node_modules/xo/cli.js'
+  const filepath = path.join(__dirname, '../node_modules/xo/cli.js')
   const process = spawn('node', [filepath, '--fix'])
   process.stdout.on('data', data => logger.error(data))
   process.stderr.on('data', data => logger.error(data))
