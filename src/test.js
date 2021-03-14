8dbada2fc5d46952e8e1a137265989ab691215f5
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-03-14 14:01:36 +0100

    use the correct mocha dir

--- src/test.js
+++ src/test.js
@@ -3,7 +3,7 @@ const { execFile } = require('./utils')
 
 async function test(options = []) {
   logger.log('starting unit tests via Mocha...')
-  const bin = `${process.cwd()}/node_modules/mocha/bin/mocha`
+  const bin = `${process.cwd()}/node_modules/shuunen-stack/node_modules/mocha/bin/mocha`
   options.unshift(bin)
   execFile(...options)
 }
