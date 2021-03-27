156a600491913ca4115b5eb7ab63e1874739759d
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-03-27 18:58:48 +0100

    use stack lint rules by default

--- src/lint.js
+++ src/lint.js
@@ -1,11 +1,14 @@
 const { logger } = require('./logger')
 const { stackFolder, asyncExec } = require('./utils')
 const path = require('path')
+const { pathExistsSync } = require('fs-extra')
 
 async function xo() {
   const formatter = require('eslint-formatter-pretty')
   const target = process.cwd() + '\\**/*.*'
-  const rules = require(path.join(process.cwd(), '.eslintrc.rules.js'))
+  let customRules = path.join(process.cwd(), '.eslintrc.rules.js')
+  if (!pathExistsSync(customRules)) customRules = path.join(stackFolder, '.eslintrc.rules.js')
+  const rules = require(customRules)
   const ignores = ['**/*.global.js']
   const options = { fix: true, space: true, semicolon: false, rules, ignores, cwd: stackFolder, extension: ['ts', 'js'] }
   const xo = require('xo')
@@ -22,7 +25,8 @@ async function xo() {
 async function repoCheck() {
   logger.consoleLogAllowed = true
   const cli = path.join(process.cwd(), 'node_modules/repo-check/dist/cli.js')
-  await asyncExec('node ' + cli)
+  const code = await asyncExec('node ' + cli)
+  process.exit(code)
 }
 
 async function lint() {
