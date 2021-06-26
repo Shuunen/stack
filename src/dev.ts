0725bd5d4b12c13392aa21c5554940beb402c619
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-06-26 16:03:56 +0200

    dev task now rely on build task

--- src/dev.ts
+++ src/dev.ts
@@ -1,37 +1,13 @@
-import { watch } from 'chokidar'
 import { lstat } from 'fs/promises'
-import { debounce } from 'shuutils'
 import { build } from './build'
 import { logger } from './logger'
 import { serve } from './serve'
-import { execFile, untilUserStop } from './utils'
 
-const execFileDebounced = debounce(execFile, 200)
-
-async function watchJsFile (file: string) {
-  execFileDebounced(file).catch(error => logger.error(error))
-  watch(file).on('all', async () => execFileDebounced(file).catch(error => logger.error(error)))
-  await untilUserStop()
-}
-
-async function watchJsFolder (folder: string) {
-  logger.debug('watchJsFolder :', folder)
-  watch(folder).on('all', async (_event, filename) => execFileDebounced(filename).catch(error => logger.error(error)))
-  await untilUserStop()
-}
-
-async function watchFile (file: string) {
-  logger.debug('watchFile :', file)
-  if (file.includes('.js')) return watchJsFile(file).catch(error => logger.error(error))
-  build([file, '--watch --silent']).catch(error => logger.error(error))
-  await watchJsFolder('dist')
-}
-
-export async function dev (options: string[]): Promise<void> {
-  if (options === undefined || options.length === 0) throw new Error('can\'t dev without input')
-  const input = options[0]
+export async function dev (args: string[]): Promise<void> {
+  const input = args[0]
+  if (!input) return build(['--dev']).catch(error => logger.error(error))
   const stat = await lstat(input)
   if (stat.isDirectory()) return serve(input)
-  if (stat.isFile()) return watchFile(input)
+  if (stat.isFile()) build([input, '--watch --run']).catch(error => logger.error(error))
   throw new Error('un-handled dev case')
 }
