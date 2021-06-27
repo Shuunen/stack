e33700164cbb14a8557fa871a36969c420c7e581
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-06-27 13:53:18 +0200

    remove confusing dev task & dev param

--- src/build-run-plugin.ts
+++ src/build-run-plugin.ts
@@ -1,5 +1,4 @@
 import { PluginBuild } from 'esbuild'
-import { lstatSync } from 'fs'
 import { logger } from './logger'
 import { execFile, exitWithError, join } from './utils'
 
@@ -10,9 +9,7 @@ export const runPlugin = {
     const filename = (entryPoints[0].match(/([\w-]+)\.[jt]s$/) || [])[1]
     if (!filename) exitWithError('failed to estimate output filename')
     const file = join(outdir, filename + '.js')
-    const stats = lstatSync(file)
-    if (!stats.isFile()) exitWithError('failed to estimate output file')
-    logger.debug('output file', file)
+    logger.debug('expected output file', file)
     build.onEnd(status => {
       if (status.errors.length > 0) exitWithError(status.errors)
       console.log('')
--- src/build.ts
+++ src/build.ts
@@ -1,5 +1,6 @@
 import postCssPlugin from '@deanc/esbuild-plugin-postcss'
 import { build as esbuild, Format, Platform } from 'esbuild'
+import { writeFileSync } from 'fs'
 import tailwindcss from 'tailwindcss'
 import { runPlugin } from './build-run-plugin'
 import { logger } from './logger'
@@ -10,37 +11,44 @@ export async function build (args: string[]): Promise<void> {
 
   const input = /([\w-]+)\.[jt]s$/.test(args[0]) ? args[0] : 'src/index.ts'
   const options = args.join(' ')
-  const platform = String((/--platform[\s=](\S*)/.exec(options) ?? [undefined, 'browser'])[1]) as Platform
+  const run = options.includes('--run')
+  const platform = String((/--platform[\s=](\S*)/.exec(options) ?? [undefined, run ? 'node' : 'browser'])[1]) as Platform
   const isBrowser = platform === 'browser'
   const outDirectory = String((/--out-dir[\s=](\S*)/.exec(options) ?? [undefined, isBrowser ? 'public' : 'dist'])[1])
   const format = String((/--format[\s=](\S*)/.exec(options) ?? [undefined, isBrowser ? 'iife' : 'cjs'])[1]) as Format
-  const dev = options.includes('--dev')
-  const minify = dev ? false : options.includes('--minify')
-  const watch = dev || options.includes('--watch')
-  const sourcemap = dev || options.includes('--sourcemap')
-  const define = (options.includes('--no-global') || isBrowser) ? { global: 'window' } : {}
+  const minify = !options.includes('--no-minify')
+  const watch = options.includes('--watch')
+  const metafile = options.includes('--meta')
+  const doServe = options.includes('--serve')
+  const sourcemap = options.includes('--sourcemap') || (isBrowser && doServe)
+  const define = (!options.includes('--no-global') && isBrowser) ? { global: 'window' } : {}
+  const plugins = isBrowser ? [postCssPlugin({ plugins: [tailwindcss] })] : []
 
-  const plugins = [postCssPlugin({ plugins: [tailwindcss] })]
-  if (options.includes('--run')) plugins.push(runPlugin)
+  if (run) plugins.push(runPlugin)
 
-  if (dev) serve(outDirectory).catch(error => logger.error(error))
+  if (doServe) serve(outDirectory).catch(error => logger.error(error))
 
-  const status = await esbuild({
+  const config = {
     bundle: true,
     define,
     entryPoints: [input],
     format,
+    metafile,
     minify,
     outdir: outDirectory,
     platform,
     plugins,
     sourcemap,
     watch,
-  })
+  }
+  logger.debug('build options', options)
+  logger.debug('esbuild config', config)
+  const status = await esbuild(config)
 
   if (status.errors.length > 0) exitWithError(status.errors)
   if (status.warnings.length > 0) logger.log('compilation ended with warnings :', status.warnings)
-  else if (watch && !dev) await untilUserStop()
-  else logger.log('\ncompilation ended successfully')
+  else if (watch) return untilUserStop()
+  logger.log('\ncompilation ended successfully')
+  if (status.metafile) writeFileSync('meta.json', JSON.stringify(status.metafile))
 }
 
